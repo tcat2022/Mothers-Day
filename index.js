0db388a08c6fec3e2b1c3537060e1b9252cc7fac
tommy <tcatty7@gmail.com>
2024-05-07 11:23:11 -0400

    added damping

--- index.js
+++ index.js
@@ -8,39 +8,42 @@ const renderer  = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(w, h);
 document.body.appendChild( renderer.domElement );
 
-const fov = 100;
+const fov = 70;
 const aspect = w / h;
 const near = 0.1;
 const far = 10;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 7;
+camera.position.z = 6;
 const scene = new THREE.Scene();
 
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.minPolarAngle = Math.PI/2;
 controls.maxPolarAngle = Math.PI/2;
+controls.enableDamping = true;
+let textMaterial;
+let textMesh;
 
 var fontLoader = new FontLoader();
 
         fontLoader.load('https://threejs.org/examples/fonts/helvetiker_regular.typeface.json', function(font) {
       
-            var textGeometry = new TextGeometry("Happy Mothers Day", {
+            var textGeometry = new TextGeometry( 'Happy ' + 'Mothers ' + '\nDay', {
                 font: font,
-                size:1.2,
-                height: .5,
+                size:.5,
+                height: .4,
                 depth: 0,
             });
 
-           const textMaterial = new THREE.MeshNormalMaterial();
-           const textMesh = new THREE.Mesh(textGeometry, textMaterial)
-          textMesh.position.x = -5;
+            textMaterial = new THREE.MeshNormalMaterial();
+            textMesh = new THREE.Mesh(textGeometry, textMaterial)
+          textMesh.position.x = -2;
 
             scene.add(textMesh)
         });
 
 function animate() {
 	requestAnimationFrame( animate );
-
+controls.update()
 	renderer.render( scene, camera );
 }
 
