6fbc1a0faf5f115d55b7b4a04cb08c7bf2cfb1cf
tommy <tcatty7@gmail.com>
2024-05-07 09:55:42 -0400

    got horizontal axis locked

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const renderer  = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(w, h);
 document.body.appendChild( renderer.domElement );
 
-const fov = 75;
+const fov = 100;
 const aspect = w / h;
 const near = 0.1;
 const far = 10;
@@ -17,21 +17,23 @@ camera.position.z = 7;
 const scene = new THREE.Scene();
 
 const controls = new OrbitControls(camera, renderer.domElement)
+controls.minPolarAngle = Math.PI/2;
+controls.maxPolarAngle = Math.PI/2;
 
 var fontLoader = new FontLoader();
 
         fontLoader.load('https://threejs.org/examples/fonts/helvetiker_regular.typeface.json', function(font) {
       
-            var textGeometry = new TextGeometry("Hello World", {
+            var textGeometry = new TextGeometry("Happy Mothers Day", {
                 font: font,
-                size: 1,
+                size:1.2,
                 height: .5,
                 depth: 0,
             });
 
            const textMaterial = new THREE.MeshNormalMaterial();
            const textMesh = new THREE.Mesh(textGeometry, textMaterial)
-          textMesh.position.x = -4;
+          textMesh.position.x = -5;
 
             scene.add(textMesh)
         });
